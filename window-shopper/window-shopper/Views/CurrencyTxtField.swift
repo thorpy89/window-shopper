b7dcfe263348a13655f4291ccab9e6c6078bc6a0
David <thorpy89@icloud.com>
2017-08-15 20:12:25 +0100

    finished lesson

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -10,8 +10,23 @@ import UIKit
 
 @IBDesignable
 
-class CurrencyTxtField: UITextField {
 
+class CurrencyTxtField: UITextField {
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: frame.size.height / 2 - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8571261764, green: 0.8509306908, blue: 0.8618698716, alpha: 0.7200429137)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let fomatter = NumberFormatter()
+        fomatter.numberStyle = .currency
+        fomatter.locale = .current
+        currencyLbl.text = fomatter.currencySymbol
+        addSubview(currencyLbl)
+        
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -27,6 +42,7 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         
         if let p = placeholder {
